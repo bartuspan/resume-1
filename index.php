0c65a46d662c35d90e583366b2b4318a789a75ab
jsawczuk <jsawczuk@b335a18d-3fc6-4ee4-9a62-b6e59ce19e93>
2010-06-05 02:27:00 +0000

    Import.

---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>James J. Sawczuk - Developer</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+
+<link href="fonts/font.css" rel="stylesheet" media="screen,print" type="text/css" />
+<link href="style.css" rel="stylesheet" media="all" type="text/css" />
+
+</head>
+<body>
+
+<div id="print_me">
+	<a href="javascript: void(0);" onclick="window.print();" title="Print this resume">Print this resum&#233;</a>.
+</div>
+
+<div id="container">
+	<h2>
+		350 Powell Rd. #1823 <span class="dot">&middot;</span> Columbia, SC 29203<br />
+		<b>phone</b> (440) 796-7806 <span class="dot">&middot;</span> <b>e-mail</b> me@jimmysawczuk.com <span class="dot">&middot;</span> <b>web</b> <a href="http://www.jimmysawczuk.com" target="_blank">www.jimmysawczuk.com</a>
+	</h2>
+	<h1>James J. Sawczuk</h1>
+	
+	<h3>Objective</h3>
+	<p class="objective">
+		To work for a dynamic, growing company and develop cutting-edge software using the latest technologies.
+	</p>
+	
+	<h3>Education</h3>
+		<div class="education_block">
+			<div class="dates">
+				8/2005 – 5/2009
+			</div>
+			
+			<div class="school">
+				Case Western Reserve University
+			</div>
+			
+			<div class="city">
+				Cleveland, OH
+			</div>
+
+			<div class="degree">
+				Bachelor of Science, Computer Science<br />
+				Minor, Mathematics
+			</div>
+			
+			<ul class="extra">
+				<li>Courses taken include: software/web engineering, databases, operating systems, networking, artificial intelligence, cryptography</li>
+				<li>Acted as team leader on several large team projects, including a data-driven website project</li>
+				<li>GPA: 3.42</li>
+			</ul>
+		</div>
+		
+		<div class="education_block">
+			<div class="dates">
+				8/2001 – 6/2005
+			</div>
+			
+			<div class="school">
+				Perry High School
+			</div>
+
+			<div class="city">
+				Perry, OH
+			</div>
+			
+			<div class="degree">
+				Honors Diploma – GPA: 3.92
+			</div>
+		</div>
+
+	<h3>Experience</h3>
+		<div class="experience_block">
+			<div class="dates">
+				6/2009 – present
+			</div>
+
+			<div class="company">
+				Louddoor, LLC
+			</div>
+
+			<div class="city">
+				Columbia, SC
+			</div>
+			
+			<div class="title">
+				Developer
+			</div>
+			
+			<ul class="extra">
+				<li>Developed Facebook platform apps on spec. for clients and superiors that are highly scalable</li>
+				<li>Used a LAMP stack, plus memcache and a load-balanced EC2 setup to deploy highly visible and highly used applications</li>
+				<li>Helped to write several Wordpress plugins to help promote blog articles on Facebook</li>
+				<li>Involved in several projects from idea to completion</li>
+				<li>Worked in a highly agile and fast-moving environment, used to changes as work progressed</li>
+				<li>Spoke at a local tech group about Facebook platform development</li>
+			</ul>
+		</div>
+		
+		<div class="experience_block">
+			<div class="dates">
+				6/2007 – 5/2009
+			</div>
+
+			<div class="company">
+				Lazorpoint, LLC
+			</div>
+
+			<div class="city">
+				Cleveland, OH
+			</div>
+
+			<div class="title">
+				Application Developer - Intern<br />
+				Client Services Engineer - Intern
+			</div>
+
+			<ul class="extra">
+				<li>Assisted clients with basic PC issues as well as network, server and printer issues</li>
+				<li>Made recommendations for hardware and software upgrades for various clients</li>
+				<li>Continually monitored clients’ networks to ensure continued stability</li>
+				<li>Worked with other engineers to standardize a network monitoring system</li>
+				<li>Created custom, data-driven applications for various clients</li>
+				<li>Gained experience in .NET development, report development and handheld development</li>
+				<li>Worked with other developers to standardize practices including application deployment techniques, version control management, and product testing standards</li>
+			</ul>
+		</div>
+		
+		<div class="experience_block">
+			<div class="dates">
+				5/2006 – 7/2006
+			</div>
+
+			<div class="company">
+				City of Mentor
+			</div>
+
+			<div class="city">
+				Mentor, OH
+			</div>
+
+			<div class="title">
+				Engineering Intern
+			</div>
+
+			<ul class="extra">
+				<li>Did field inspection work with city engineers</li>
+				<li>Ran microfilm archival project</li>
+				<li>Provided technical advice on electronic data storage formats</li>
+				<li>Presented status and other information at department meetings</li>
+			</ul>
+		</div>
+		
+		<div class="experience_block">
+			<div class="dates">
+				11/2003 – 8/2005
+			</div>
+			
+			<div class="company">
+				Perry Local Schools IS Department
+			</div>
+			
+			<div class="city">
+				Perry, OH
+			</div>
+			
+			<div class="title">
+				Web Programmer/Designer
+			</div>
+
+			<ul class="extra">
+				<li>Content management and addition for faculty and staff</li>
+				<li>Worked with other students on several larger projects, including a high school course registrar and a course database complete with objectives</li>
+			</ul>
+		</div>
+		
+	<h3>Skills</h3>
+		<ul class="normal_list">
+			<li><b>Office programs:</b> MOUS (Microsoft Office User Specialist) certification: Word, Excel, and Access, extensive experience with Powerpoint, Sharepoint</li>
+			<li><b>Development programs:</b> Extensive experience with Visual Studio, familiar with Adobe Creative Suite (Dreamweaver, Fireworks)</li>
+			<li><b>Programming languages:</b> Extensive experience with C#, PHP, HTML/XHTML, CSS , experience in C++, Python, Javascript/jQuery</li>
+			<li><b>Databases:</b> Extensive experience with MySQL queries, administration; extensive experience with SQL Server 2005 queries, Reporting Services development</li>
+			<li><b>Operating Systems:</b> Extensive experience with Windows 98 through Windows Vista, experience in Mac OS X, experience in Linux (desktop and programming)</li>
+			<li><b>Other:</b> Extensive experience with the Facebook platform, experience with Mathematica, experience with Subversion and CVS version control, experience with Memcache, experience with Sharepoint application/workflow development</li>
+		</ul>
+		
+	<h3>Other</h3>
+		<ul class="normal_list">
+			<li>Created a small blog website that operated for almost four years. Users were allowed to upload photos, write journal entries, etc. Source available upon request.</li>
+			<li>Senior project at Case is a large application that has web application, web services application, client and iPhone client components. The idea is to synchronize information for all users across a variety of platforms and services. Formerly located at http://www.zaphiri.com.</li>
+			<li>Contractual work for several small companies and organizations, including a sorority at my university (which is still online) and a Goddard School branch</li>
+			<li>Created and currently administer a website frontend and Google Apps-based backend of a local church for about 15 users, including e-mail, calendars, chat and a custom website</li>
+			<li><b>Microsoft Certified Professional ID</b>: 6635414</li>
+		</ul>
+</div>
+
+<p id="footer">Last modified on <?=date("F j, Y", filemtime("/home/jsawczuk/files.jimmysawczuk.com/resume/index.php")); ?></p>
+
+<? /*<script type="text/javascript" charset="utf-8" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> */ ?>
+</body>
+</html>
